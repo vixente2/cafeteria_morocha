f1d5477b052a59758d908af2e30b5d2f80c79086
vixente2 <carlos.lara2@virginiogomez.cl>
2026-06-07 19:12:07 -0400

    Se agrego views de productos y mesas

--- appMorocha/urls.py
+++ appMorocha/urls.py
@@ -8,10 +8,16 @@ urlpatterns = [
     path('estadoPedido/',views.estadoPedido, name='estadoPedido'),
     path('usuario/',views.usuario),
     path('registrarPedido/',views.registrarPedido, name='registrarPedido'),
+    path('ingresarProducto/',views.ingresarProducto, name='ingresarProducto'),
+    path('ingresarMesa/',views.ingresarMesa, name='ingresarMesa'),
     # Rutas para las acciones
     path('detallePedido/<int:id_pedido>/',views.detallePedido, name='detallePedido'),
     path('eliminarPedido/<int:id_pedido>/', views.eliminarPedido, name='eliminarPedido'),
+    # Guardar datos de usuarios y liminar usuario
     path('procesarUsuario/',views.procesarUsuario),
     path('eliminarUsuario/<str:nombreUsuario>/',views.eliminarUsuario,name='eliminarUsuario'),
+    # Guardar datos de productos
+    path('procesarProducto/',views.procesarProducto),
+   
 
 ]
