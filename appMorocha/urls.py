79b30fc81e1b80dcc333db24cedd7e6508870c06
vixente2 <carlos.lara2@virginiogomez.cl>
2026-06-10 00:00:26 -0400

    cambios en estado pedidos

--- appMorocha/urls.py
+++ appMorocha/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('registrarPedido/',views.registrarPedido, name='registrarPedido'),
     path('ingresarProducto/',views.ingresarProducto, name='ingresarProducto'),
     path('ingresarMesa/',views.ingresarMesa, name='ingresarMesa'),
+    path('listarPedidos/',views.listarPedidos, name='listarPedidos'),
     # Acciones para Pedidos
-    path('detallePedido/<int:id_pedido>/',views.detallePedido, name='detallePedido'),
     path('eliminarPedido/<int:id_pedido>/', views.eliminarPedido, name='eliminarPedido'),
     # Acciones para Usuario
     path('procesarUsuario/',views.procesarUsuario),
@@ -20,7 +20,8 @@ urlpatterns = [
     # Acciones para Productos
     path('procesarProducto/',views.procesarProducto),
     path('editarProducto/',                      views.editarProducto,    name='editarProducto'),
-    path('eliminarProducto/<int:id_producto>/',  views.eliminarProducto,  name='eliminarProducto')
-   
+    path('eliminarProducto/<int:id_producto>/',  views.eliminarProducto,  name='eliminarProducto'),
+   # Acciones para Mesa
+    path('editarMesa/',views.editarMesa),
 
 ]
