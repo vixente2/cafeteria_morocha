d5dcc3843deb40af9e41bb2bd52efaa923999206
emersonec1 <emerson.erices@virginiogomez.cl>
2026-06-09 22:54:15 -0400

    Login funcional, CRUD productos funcional, solucion de bugs

--- appMorocha/static/appMorocha/js/procesarUsuario.js
+++ appMorocha/static/appMorocha/js/procesarUsuario.js
@@ -1,46 +1,46 @@
-$(document).ready(function(){
-    $('.btnEliminar').click(function(){
-        let nombreUsuario=$(this).data('usuario');
-        let clave=$(this).data('clave');
+$(document).ready(function () {
+    $('.btnEliminar').click(function () {
+        let nombreUsuario = $(this).data('usuario');
+        let clave = $(this).data('clave');
         Swal.fire({
             title: "Eliminar Registro",
-            text: "¿Estás seguro que quieres eliminar el registro de "+nombreUsuario+" ?",
+            text: "¿Estás seguro que quieres eliminar el registro de " + nombreUsuario + " ?",
             icon: "warning",
             showCancelButton: true,
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
-            cancelButtonText:"No",
+            cancelButtonText: "No",
             confirmButtonText: "Si, Eliminar"
-            }).then((result) => {
-            if (result.isConfirmed) 
-                window.location.href="/eliminarUsuario/"+nombreUsuario
+        }).then((result) => {
+            if (result.isConfirmed)
+                window.location.href = "/eliminarUsuario/" + nombreUsuario
         });
     });
-    
-    $('.btnVerMas').click(function(){
-    let idUsuario = $(this).data("id");
-    let nombreUsuario = $(this).data("usuario");
-    let rol = $(this).data("rol");
-    let clave = $(this).data("clave");
-    Swal.fire({
-        title:"Información",
-        icon:'info',
-        html:`
+
+    $('.btnVerMas').click(function () {
+        let idUsuario = $(this).data("id");
+        let nombreUsuario = $(this).data("usuario");
+        let rol = $(this).data("rol");
+        let clave = $(this).data("clave");
+        Swal.fire({
+            title: "Información",
+            icon: 'info',
+            html: `
             <table class="table table-bordered">
                 <tr>
-                    <td>ID</td>
+                    <td><strong>ID</strong></td>
                     <td>${idUsuario}</td>
                 </tr>
                 <tr>
-                    <td>Usuario</td>
+                    <td><strong>Usuario</strong></td>
                     <td>${nombreUsuario}</td>
                 </tr>
                 <tr>
-                    <td>Rol</td>
+                    <td><strong>Rol</strong></td>
                     <td>${rol}</td>
                 </tr>
                 <tr>
-                    <td>Clave</td>
+                    <td><strong>Clave</strong></td>
                     <td>${clave}</td>
                 </tr>
             </table>
